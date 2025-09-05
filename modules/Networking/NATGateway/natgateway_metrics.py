4bb6fc8ae97663fb30268f76a55e30f45fd53fb9
haimd-gloat <ext.haim.dagan@gloat.com>
2025-09-05 23:04:02 +0300

    bug fix

--- modules/Networking/NATGateway/natgateway_metrics.py
+++ modules/Networking/NATGateway/natgateway_metrics.py
@@ -3,6 +3,36 @@ from utils.utils import save_as_file_parquet_metrics, generate_parquet_prefix, g
 
 
 def natgateway_metrics(file_path, session, region, account, metrics, time_generated, boto_config):
+    """
+    The function `natgateway_metrics` retrieves and saves metrics for NAT gateways in a specified region
+    and account.
+    
+    :param file_path: The `file_path` parameter is the file path where the metrics data will be saved.
+    It is a string that represents the location where the metrics will be stored, such as a file path on
+    the local system or a cloud storage location
+    :param session: The `session` parameter in the `natgateway_metrics` function is typically an
+    instance of a boto3 session that is used to create clients and resources for AWS services. It allows
+    you to configure credentials, region, and other settings for interacting with AWS services
+    :param region: Region is a string representing the AWS region where the NAT gateways are located. It
+    specifies the geographical area where the resources are deployed, such as 'us-east-1' for US East
+    (N. Virginia) or 'eu-west-1' for EU (Ireland)
+    :param account: The `account` parameter in the `natgateway_metrics` function seems to be a
+    dictionary containing information about the AWS account. It likely includes details such as the
+    account ID, which can be accessed using `account['account_id']`. This information is used within the
+    function to identify the account associated with
+    :param metrics: The `metrics` parameter in the `natgateway_metrics` function likely refers to a data
+    structure or object that stores information related to metrics for NAT gateways. This could include
+    metrics such as network utilization, data transfer rates, error rates, or any other relevant
+    performance indicators for NAT gateways. The
+    :param time_generated: The `time_generated` parameter in the `natgateway_metrics` function
+    represents the timestamp or time at which the metrics are generated or collected. This parameter is
+    used to track when the metrics data was obtained for the NAT gateways in the specified AWS account
+    and region. It helps in organizing and analyzing the
+    :param boto_config: Boto3 configuration that includes settings such as retries, timeouts, and other
+    configurations for the AWS SDK for Python (Boto3). It allows you to customize the behavior of the
+    Boto3 client when making API calls to AWS services. You can pass this configuration to the Boto3
+    client to
+    """
     next_token = None
     idx = 0
     client = session.client('ec2', region_name=region, config=boto_config)
--- modules/Storage/EFS/efs_metrics.py
+++ modules/Storage/EFS/efs_metrics.py
@@ -3,6 +3,31 @@ from utils.utils import save_as_file_parquet_metrics, generate_parquet_prefix, g
 
 
 def efs_filesystem_metrics(file_path, session, region, account, metrics, time_generated, boto_config):
+    """
+    This Python function retrieves metrics for EFS file systems and saves them in a Parquet file.
+    
+    :param file_path: The `file_path` parameter in the `efs_filesystem_metrics` function is the path
+    where the metrics data will be saved as a file. It is the location where the Parquet file containing
+    the metrics will be stored
+    :param session: The `session` parameter in the `efs_filesystem_metrics` function is typically an
+    instance of a boto3 session that allows you to create service clients and resources for AWS
+    services. It is used to interact with the AWS Elastic File System (EFS) service in this function
+    :param region: Region is a string representing the AWS region where the EFS file system is located.
+    It specifies the geographical area where the resources are deployed
+    :param account: The `account` parameter in the `efs_filesystem_metrics` function seems to represent
+    an account object that likely contains information about the account, such as the account ID. It is
+    used to extract the account ID from the account object using `account['account_id']` in the function
+    :param metrics: Metrics is a dictionary that stores the metrics data related to the EFS filesystems.
+    It is used to track and store various performance and utilization metrics of the EFS filesystems
+    :param time_generated: The `time_generated` parameter in the `efs_filesystem_metrics` function is
+    used to specify the timestamp or time at which the metrics are generated or collected. This
+    timestamp is important for tracking when the metrics data was collected and can be used for analysis
+    or comparison purposes. It helps in understanding the time
+    :param boto_config: The `boto_config` parameter in the `efs_filesystem_metrics` function is used to
+    provide additional configuration options for the AWS SDK for Python (Boto3) client that is created
+    within the function. This configuration can include settings such as retries, timeouts, and other
+    client-specific configurations
+    """
     next_token = None
     idx = 0
     client = session.client('efs', region_name=region, config=boto_config)
